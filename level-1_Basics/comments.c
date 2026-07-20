56215c53633a0f9eccab33d4ceeb9f8a5ba426dd
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-20 16:39:10 +0530

    about comments

---
+++ level-1_Basics/comments.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    // This is a single-line comment in C
+    printf("This is a comment in C\n");
+    /* this is a multi-line comment in C */
+    /* It can span multiple lines
+       and is enclosed between /* and */
+      /* ignored by the compiler*/
+    printf("Comments are useful for explaining code\n");
+
+    return 0;
+}
