5e86cc8d375307378904ed4a7306348195bbcf4e
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-20 17:52:27 +0530

    compilation error

---
+++ level-1_Basics/const1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    const int age = 5;
+    
+    printf("The value of age is: %d\n", age);
+    /*age = 10; 
+    printf("The new value of age is: %d\n", age);
+    // This would cause a compilation error*/
+    return 0;
+}
