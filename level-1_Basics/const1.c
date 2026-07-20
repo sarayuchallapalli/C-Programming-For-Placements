5856927bf1c16013857cd8e1bff8294474c2d56e
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-20 17:59:38 +0530

    solving

--- level-1_Basics/const1.c
+++ level-1_Basics/const1.c
@@ -3,10 +3,15 @@
 int main()
 {
     const int age = 5;
-    
+
     printf("The value of age is: %d\n", age);
     /*age = 10; 
     printf("The new value of age is: %d\n", age);
     // This would cause a compilation error*/
+    const int x = 10;
+    int y = x;// y is not a constant, so it can be modified
+    printf("%d\n", y);
+    y = 20;
+    printf("The new value of y is: %d\n", y);
     return 0;
 }
