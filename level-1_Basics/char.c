d02a02accec9d3c1386d2e4b28b6613dee5e6f6a
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-20 17:21:51 +0530

    solving

--- level-1_Basics/char.c
+++ level-1_Basics/char.c
@@ -5,5 +5,8 @@ int main()
     char ch = '1';
     printf("The value of ch is: %c\n", ch);// prints the char "1"
     printf("The value of ch is: %d\n", ch);// prints the ASCII value of char "1" which is 49
+    char sa = 66;
+    printf("%c\n", sa);//B
+    printf("%d\n", sa);//66
     return 0;
 }
