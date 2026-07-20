4c4aaac86670e4395607009fd249893e4f1a824a
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-20 16:52:51 +0530

    variables

---
+++ level-1_Basics/variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+ {
+    int age = 5;
+    printf("The value of age is: %d\n", age);
+    age = 10;
+    printf("The new value of age is: %d\n", age);
+    return 0;
+}
