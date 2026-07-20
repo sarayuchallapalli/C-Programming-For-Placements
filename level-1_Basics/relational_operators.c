a5c1aa75a9933fda962f8238eeaecd2990ead696
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-20 19:38:21 +0530

    operators

--- level-1_Basics/relational_operators.c
+++ level-1_Basics/relational_operators.c
@@ -3,7 +3,7 @@
 int main()
 {
     int a,b;
-    
+
     printf("Enter two numbers: ");
     scanf("%d %d",&a,&b);
     printf("a > b: %d\n", a > b);
@@ -12,5 +12,11 @@ int main()
     printf("a != b: %d\n", a != b);
     printf("a >= b: %d\n", a >= b);
     printf("a <= b: %d\n", a <= b);
+
+    int c = 10;
+
+    printf("%d\n", c = 5);
+    printf("%d\n", c == 5);
     return 0;
+
 }
