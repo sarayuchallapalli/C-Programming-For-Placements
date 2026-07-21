fc41e81af0b195b134db5b70cb22a229741b23e6
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-21 12:50:22 +0530

    decrement operators

---
+++ level-1_Basics/decrement_operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int a = 5;
+    int b = a--;
+    
+    printf("%d\n", a--);
+    printf("%d %d\n", a,b);
+    b = --a;
+    printf("%d\n", --a);
+    printf("%d %d\n", a,b);
+    printf("%d\n",a = a-- + --a);//in standard C, the behavior of this expression is undefined because it modifies 'a' more than once between sequence points. The result may vary depending on the compiler and optimization settings.
+    return 0;
+}
