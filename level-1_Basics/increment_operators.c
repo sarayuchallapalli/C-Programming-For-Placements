0e56920654212c9fac555b3b761fd00d29b08ecd
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-21 12:48:07 +0530

    incrememt operators

--- level-1_Basics/increment_operators.c
+++ level-1_Basics/increment_operators.c
@@ -3,10 +3,13 @@
 int main()
 {
     int a = 5;
+    int b = a++;
     printf("%d\n", a++);
-    printf("%d\n", a);
+    printf("%d %d\n", a,b);
+    b = ++a;
     printf("%d\n", ++a);
-    printf("%d\n", a);
+    printf("%d %d\n", a,b);
+
     printf("%d\n",a = a++ + ++a);//in standard C, the behavior of this expression is undefined because it modifies 'a' more than once between sequence points. The result may vary depending on the compiler and optimization settings.
     return 0;
 }
