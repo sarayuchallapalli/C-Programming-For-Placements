d81c3b6e276ea4323365402eed64f9830f7c9c58
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-23 15:59:11 +0530

    continue

---
+++ level-3_loops/continue.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int i;
+
+    for(i = 1; i <= 5; i++)
+{
+    if(i == 3)
+    {
+        continue;//continue statement will skip the current iteration when i is equal to 3
+    }
+
+    printf("%d ", i);
+}
+printf("\n");
+for(i = 1; i <= 5; i++)
+    {
+        if(i % 2 == 0)
+        {
+            continue;
+        }
+
+        printf("%d ", i);
+
+        printf("B ");
+    }
+
+    printf("\n%d", i);
+    return 0;
+}
