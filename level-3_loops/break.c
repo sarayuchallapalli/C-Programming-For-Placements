ecab4f3fc43e4d6fa44ad5165737431b1496fc84
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-23 12:55:11 +0530

    break

---
+++ level-3_loops/break.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+int main()
+
+{
+     int i;
+     for(i = 1; i <= 5; i++)
+{
+    if(i == 3)
+    {
+        break;//break statement will terminate the loop when i is equal to 3
+    }
+
+    printf("%d ", i);
+}
+
+printf("\n");
+
+    for(int i = 1; i <= 3; i++)
+    {
+        for(int j = 1; j <= 3; j++)
+        {
+            printf("%d%d ", i, j);
+
+            if(j == 2)
+            {
+                break;//break statement will terminate the inner loop when j is equal to 2
+            }
+        }
+
+        printf("\n");
+    }
+    for(int i = 1; i <= 3; i++)
+{
+    for(int j = 1; j <= 3; j++)
+    {
+        if(j == 2)
+        {
+            break;
+        }
+
+        printf("%d%d ", i, j);
+    }
+
+    printf("\n");
+}
+
+    return 0;
+
+}
