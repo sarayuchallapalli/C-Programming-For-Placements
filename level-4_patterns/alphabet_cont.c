0077b782330670387d33cc0030c8d575a25dae6e
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-27 20:08:56 +0530

    continous alphabet pattern

---
+++ level-4_patterns/alphabet_cont.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int main()
+
+{
+    
+    char ch ='A';
+    for(int i =1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+        
+            printf("%c",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
