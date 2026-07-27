f5b69aeedfd61dfd4574f3a3338f8333a0323871
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-27 18:54:35 +0530

    hallow pyramid

---
+++ level-4_patterns/hallow_pyramid.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int main()
+{
+
+
+    for(int i =1; i<=4; i++){
+        for(int j = 1; j <= 4-i; j++){
+               printf(" ");
+        }
+        for (int k = 1; k<=2*i-1;k++){
+            if(k==1 || k==2*i-1 || i==4)
+                    printf("*");
+    else 
+        printf(" ");
+        }
+    printf("\n");
+    }
+    
+    return 0;
+}
