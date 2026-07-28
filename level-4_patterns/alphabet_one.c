fadfec648640b32a918c0b74bba0ef632c6fec4f
Sarayu Challapalli <sarayuchallapalli@gmail.com>
2026-07-28 16:33:43 +0530

    patterns

---
+++ level-4_patterns/alphabet_one.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    for(int i = 1;i<=4;i++){
+        char ch = 'A'+i-1;
+        for(int j=1;j<=i;j++){
+            printf("%c",ch);
+            ch--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
